4e6050e4df1b0e08f2979b2b5eadddb261d343f7
smsohan <sohan39@gmail.com>
2016-12-19 23:34:14 -0700

    pg for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161219224823) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.integer  "task_id"
     t.integer  "participant_id"
